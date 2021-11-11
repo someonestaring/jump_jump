8710b0d1ca8a7e14664b3b8c0ffc8d9c57580f69
chip_pickering <mykeyworks.2113@gmail.com>
2021-11-11 09:48:13 -0500

    decent start: basic layout set, though buggie bugzz --> limited functionality but things are hooked up to stuff for the most part lolol

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:control_pad/control_pad.dart';
 
 void main() {
-  runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
+  SystemChrome.setPreferredOrientations([DeviceOrientation.landscapeLeft])
+      .then((_) {
+    runApp(const MyApp());
+  });
 }
 
 class MyApp extends StatelessWidget {
@@ -29,6 +36,8 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  bool _active = false;
+  bool _moving = false;
   int _score = 0;
 
   void _incrementScore() {
@@ -39,13 +48,107 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    PreferredSizeWidget? _appBar() {
-      return AppBar(
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
+    Size size = MediaQuery.of(context).size;
+    // PreferredSizeWidget? _appBar() {
+    //   return AppBar(
+    //     title: Row(
+    //       mainAxisAlignment: MainAxisAlignment.center,
+    //       children: [
+    //         const Spacer(),
+    //         Text(
+    //           widget.title,
+    //         ),
+    //         const Spacer(),
+    //         Text('Score: $_score'),
+    //       ],
+    //     ),
+    //   );
+    // }
+
+    Widget _background() {
+      return _moving
+          ? Image.asset(
+              'assets/gifs/moving_screen.gif',
+              fit: BoxFit.fitWidth,
+              width: size.width,
+            )
+          : Image.asset(
+              'assets/images/start_screen.png',
+              fit: BoxFit.fitWidth,
+              // width: size.height,
+            );
+    }
+
+    Widget _userInfo() {
+      return Padding(
+        padding: EdgeInsets.symmetric(
+            vertical: size.height * 0.02, horizontal: size.width * 0.02),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
+            // const Spacer(),
             Text(
-              widget.title,
+              'UserNameScoreStuffWhyNot: $_score',
+              style: const TextStyle(color: Colors.white70, fontSize: 24),
+            ),
+            GestureDetector(
+              onTap: () {
+                setState(() {
+                  _active = !_active;
+                  _moving = !_moving;
+                });
+              },
+              child: Icon(
+                !_active ? Icons.play_arrow : Icons.pause,
+                color: Colors.white70,
+                size: 44,
+              ),
+            ),
+          ],
+        ),
+      );
+    }
+
+    // Widget _character() {
+    //   return;
+    // }
+// Widget _obstacles() {
+//   return
+// }
+
+    Widget _joysticks() {
+      return Padding(
+        padding: EdgeInsets.only(
+          top: size.height * 0.5,
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Padding(
+              padding: EdgeInsets.only(left: size.width * 0.025),
+              child: JoystickView(
+                size: size.width * 0.15,
+                showArrows: false,
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(right: size.width * 0.025),
+              child: PadButtonsView(
+                padButtonPressedCallback: (button, tap) {
+                  switch (button) {
+                    case 0:
+                      return print('Button $button pressed you dumbie');
+                    case 1:
+                      return print('Button $button pressed you dumbie');
+                    case 2:
+                      return print('Button $button pressed you dumbie');
+                    case 3:
+                      return print('Button $button pressed you dumbie');
+                    default:
+                  }
+                },
+                size: size.width * 0.2,
+              ),
             ),
           ],
         ),
@@ -55,13 +158,19 @@ class _MyHomePageState extends State<MyHomePage> {
     Widget _bodyContent() {
       return Center(
         child: Stack(
-          children: const [],
+          children: [
+            _background(),
+            _userInfo(),
+            // _character(),
+            // _obstacles(),
+            _joysticks(),
+          ],
         ),
       );
     }
 
     return Scaffold(
-      appBar: _appBar(),
+      // appBar: _appBar(),
       body: _bodyContent(),
     );
   }
